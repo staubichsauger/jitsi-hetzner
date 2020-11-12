963b0d53dde2095599a252403e3177da7fb84d1a
staubichsauger <49568580+staubichsauger@users.noreply.github.com>
2020-11-12 22:38:23 +0100

    skip known_hosts check for scp

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-scp -r -i $(cat variables.tf | grep id_rsa\" | sed 's#.*"\(.*id_rsa.*\).*"#\1#g') root@"$(cat terraform.tfvars | grep dnsname | sed 's#dnsname = "\(.*\)"#\1#g')":~/.jitsi-meet-cfg/web/letsencrypt tmp/backup
+scp -r -o StrictHostKeyChecking=no -i $(cat variables.tf | grep id_rsa\" | sed 's#.*"\(.*id_rsa.*\).*"#\1#g') root@"$(cat terraform.tfvars | grep dnsname | sed 's#dnsname = "\(.*\)"#\1#g')":~/.jitsi-meet-cfg/web/letsencrypt tmp/backup
 
 REPO_PATH=$(pwd)
 
